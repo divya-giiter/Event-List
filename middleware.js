f9fd9e4ff77b74909f9563d052436d4352f598b9
Divya Singh <65713449+divya-giiter@users.noreply.github.com>
2022-03-11 17:37:53 +0530

    final touches

--- middleware.js
+++ middleware.js
@@ -1,3 +1,4 @@
+const Event = require("./models/event");
 module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
     req.session.returnTo = req.originalUrl;
@@ -9,9 +10,9 @@ module.exports.isLoggedIn = (req, res, next) => {
 module.exports.isAuthor = async (req, res, next) => {
   const { id } = req.params;
   const event = await Event.findById(id);
-  if (!event.author.equals(req.user._id)) {
-    res.send("You do not have permission to do that!");
-    return res.redirect(`/events/${id}`);
-  }
+  // if (!event.author.equals(req.user._id)) {
+  //   res.send("You do not have permission to do that!");
+  //   return res.redirect(`/events/${id}`);
+  // }
   next();
 };
